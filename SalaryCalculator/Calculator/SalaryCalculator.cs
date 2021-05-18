6ae76655af1e2886ad90317574ecbfa7c1cd06d3
Alican Demirtas <demirtasdev@hotmail.com>
2021-05-18 11:47:53 +0100

    GetAnnualSalary Test — The Refactor Phase

--- SalaryCalculator/Calculator/SalaryCalculator.cs
+++ SalaryCalculator/Calculator/SalaryCalculator.cs
@@ -4,9 +4,10 @@ namespace Calculator
 {
     public class SalaryCalculator
     {
+        private const int HOURS_IN_A_YEAR = 2080;
+
         public static decimal GetAnnualSalary(decimal hourlyWage)
         {
-            const int HOURS_IN_A_YEAR = 2080;
             var annualSalary = hourlyWage * HOURS_IN_A_YEAR;
             return annualSalary;
         }
